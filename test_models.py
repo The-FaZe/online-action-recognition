b01b8a7455024c37c05e16f4d9d8c9371596fd42
Phylake1337 <47036237+Phylake1337@users.noreply.github.com>
2019-02-19 09:57:54 -0930

    Update test_models.py

--- test_models.py
+++ test_models.py
@@ -162,7 +162,7 @@ for i, (data, label) in enumerate(data_loader):
                                                                     float(count_time) / (i+1)))
     indxes = np.flip(np.argsort(result[0].mean(axis=0)),axis = 1)[ : , : 5]
     
-    print('Top 5 actions: ' , indxes)
+    print('Top 5 actions : {} -- True Labal is : {} '.format(indxes,result[1]))
     
     #for k in np.argsort(result[0])[::-1][:5]:
         #print('%-22s %0.2f%%' % (Label[str(k+1)], result[0][k]))

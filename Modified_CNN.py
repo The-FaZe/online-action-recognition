5bfb8130481134914fb250180311e50ab26dd6de
Phylake1337 <47036237+Phylake1337@users.noreply.github.com>
2019-02-22 13:28:48 -0930

    Update Modified_CNN.py

--- Modified_CNN.py
+++ Modified_CNN.py
@@ -106,7 +106,7 @@ class TSN_model(nn.Module):
               if self.modality == 'RGBDiff':
                 print('Convert flow weights to RGBDiff weights')
                 new_weights = state_dict['conv1_7x7_s2.weight'].mean(dim=1,keepdim=True).expand([64,3,7,7]).contiguous()
-                frist_layer = getattr(model,'conv1_7x7_s2')
+                frist_layer = getattr(self.base_model,'conv1_7x7_s2')
                 frist_layer.weight.data = new_weights
                 frist_layer.weight.bias = state_dict['conv1_7x7_s2.bias']
               

2de3b5082997bad4f25ca9dba0b7c5301f6845bf
Abdullah <abdullah_aml@protonmail.com>
2019-03-11 00:33:02 +0200

    updating topN class to get the top N indices and to print them.

--- sliding_window_video_test.py
+++ sliding_window_video_test.py
@@ -45,26 +45,6 @@ parser.add_argument('--gpus', nargs='+', type=int, default=None)
 args = parser.parse_args()
 
 
-def label_dic(classInd):
-  """
-  returns a list of the actions ordered by the list index
-  input:
-      classInd: of type string refering the input textfile
-  output:
-      action_label: of type list
-  """
-  action_label=[]
-  with open(classInd) as f:
-      content = f.readlines()
-      
-  f.close()
-  for line in content:
-      action_label.append(line.split(' ')[-1].rstrip()) #splitting the number from the action
-                                                        #and removing the '/n' using 
-                                                        #rstrip() method
-    
-          
-  return action_label
 
 """
 --------------------Sliding Window ------------------------------------
@@ -123,48 +103,102 @@ def sliding_window_aggregation_func(score, spans=[1, 2, 4, 8, 16], overlap=0.2, 
 """
 class top_N(object):
     
-    def __init__(self, N, actions_list, scores):
+    def __init__(self, classInd_textfile, N=5):
         """
         input:
-            N: is an integer
-            action_list: of type 1d list
-            scores: of type numpay 1d array (row vector)
+            classInd_textfile: the text file that contains actions
+            N: is an integer refrering to top N actions
         """
 
         self.N = N
-        self.actions_list = actions_list
-        self.scores = scores
+        self.actions_list = self.load_actions_label(classInd_textfile)
+        self.scores = None
         
+        
+    def load_actions_label(self, classInd):
+        """
+        returns a list of the actions ordered by the list index
+        input:
+            classInd: of type string refering the input textfile
+            output:
+                action_label: of type list
+        """
+        action_label_list=[]
+        with open(classInd) as f:
+            content = f.readlines()
+      
+        f.close()
+        for line in content:
+            action_label_list.append(line.split(' ')[-1].rstrip()) #splitting the number from the action
+                                                            #and removing the '/n' using 
+                                                            #rstrip() method
+    
+          
+        return action_label_list    
+    
+    
+    def import_scores(self, scores):
+        """
+        the method impoetes the socres of the actions
+        """
+        
+        self.scores = scores
+    
     def get_top_N_actions(self):
         """
-        returns a tuple (top N actions' indicies, list of type N action)
+        input:
+            scores: of type numpay 1d array (row vector) that contains actions'
+                    scores
+        returns a tuple (top N actions' indicies of type numpy array,
+                                                     list of type N action,
+                                                     numpy array of top N cores)
+        return False if the scores were not given
         
         """
+        #Handelling no scores' input
+        try:
+            if self.scores == None:
+                return False
+        except:
+            pass
+                
         sorted_indcies = np.argsort(self.scores)[::-1] #soring the indicies from 
                                                        #the biggesr to the loewst
+                                                       
         
         sorted_indcies = sorted_indcies[:self.N]     #taking top N actioms
         
-        top_N_actions= []
+        top_N_actions = []
+        top_N_scores  = []
         for i in sorted_indcies:
             top_N_actions.append(self.actions_list[i])
+            top_N_scores.append(self.scores[i])
             
-        return sorted_indcies, top_N_actions
+        return sorted_indcies, top_N_actions, top_N_scores
     
     def __str__(self):
+        
         """
         this method will be activated when doing print(object)
         """
+        
         open_statement = "Top " + str(self.N) + " Actions.\n"
+        
+        #Handelling no scores' input
+        try:
+            if self.scores == None:
+                return open_statement + "\nThere is no scores were given."
+        except:
+            pass
+            
+        
         action_satement = ''
         
-        indcies , top_actions = self.get_top_N_actions()
+        _, top_actions, top_N_scores = self.get_top_N_actions()
         
-        j=0
-        for i in indcies:
-            action_satement += top_actions[j] + " : " \
-                            + "{0:.4f}".format(self.scores[i]*100) + '\n'
-            j+=1
+        for i in range(self.N):
+            action_satement += top_actions[i] + " : " \
+                            + "{0:.4f}".format(top_N_scores[i]*100) + '\n'
                             
         return open_statement + action_satement
             
@@ -220,7 +254,7 @@ def one_video():
   --------------------Inzializations---------------------------
   """
     
-  action_label = label_dic(args.classInd_file) #loading actions from 
+  top5_actions = top_N(args.classInd_file, 5)
 
   if args.dataset == 'ucf101':
       num_class = 101
@@ -342,8 +376,8 @@ def one_video():
   end_time = time.time() - start_time
   print("time taken: ", end_time)
   
-  top_5_actions = top_N(5, action_label, out_scores)
-  print(top_5_actions)
+  top5_actions.import_scores(out_scores) #importing scores to the class
+  print(top5_actions)
 
 
 

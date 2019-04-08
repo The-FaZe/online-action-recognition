1d99e051d6bbca19cb6e86b490dd0d9e08f561f2
Ahmed Mohamed Gamaleldin <ahmedgamal1496@gmail.com>
2019-04-08 14:03:46 +0200

    Added two streams

--- polymerization.py
+++ polymerization.py
@@ -77,10 +77,16 @@ def First_step():
     
       with torch.no_grad():
           #reshape data to be in shape of (num_segments*crop_number,length,H,W)
-          input = data.view(-1, length, data.size(1), data.size(2))
           #Forword Propagation
-          output = model(input)
-          output_np = output.data.cpu().numpy().copy()
+          if model == 'RGB':
+              input = data.view(-1, 3, data.size(1), data.size(2))
+              output = model_RGB(input)
+
+          elif model == 'RGBDiff':
+              input = data.view(-1, 18, data.size(1), data.size(2))
+              output = model_RGBDiff(input)
+      
+          output_np = output.data.cpu().numpy().copy()    
           #Reshape numpy array to (num_crop,num_segments,num_classes)
           output_np = output_np.reshape((num_crop, test_segments, num_class))
           #Take mean of cropped images to be in shape (num_segments,1,num_classes)
@@ -96,28 +102,27 @@ def First_step():
   else:
       raise ValueError('Unkown dataset: ' + args.dataset)
   
-  model = TSN_model(num_class, 1, args.modality,
+  model_RGB = TSN_model(num_class, 1, 'RGB',
                     base_model_name=args.arch, consensus_type='avg', dropout=args.dropout)
   
-  #load the weights of your model training
-  checkpoint = torch.load(args.weights)
-  print("epoch {}, best acc1@: {}" .format(checkpoint['epoch'], checkpoint['best_acc1']))
-
-  base_dict = {'.'.join(k.split('.')[1:]): v for k,v in list(checkpoint['state_dict'].items())}
-  model.load_state_dict(base_dict)
+  model_RGBDiff = TSN_model(num_class, 1, 'RGBDiff',
+                    base_model_name=args.arch, consensus_type='avg', dropout=args.dropout)
   
-  #test_crops is set to 1 for fast video evaluation
-  if args.test_crops == 1:
-      cropping = torchvision.transforms.Compose([
-          GroupScale(model.scale_size),
-          GroupCenterCrop(model.input_size),
+  for i in range(len(args.weights)):
+      #load the weights of your model training
+      checkpoint = torch.load(args.weights[i])
+      print("epoch {}, best acc1@: {}" .format(checkpoint['epoch'], checkpoint['best_acc1']))
+    
+      base_dict = {'.'.join(k.split('.')[1:]): v for k,v in list(checkpoint['state_dict'].items())}
+      if i==0:
+          model_RGB.load_state_dict(base_dict)
+      else:
+          model_RGBDiff.load_state_dict(base_dict)
+  
+  cropping = torchvision.transforms.Compose([
+          GroupScale(model_RGB.scale_size),
+          GroupCenterCrop(model_RGB.input_size),
       ])
-  elif args.test_crops == 10:
-      cropping = torchvision.transforms.Compose([
-          GroupOverSample(model.input_size, model.scale_size)
-      ])
-  else:
-      raise ValueError("Only 1 and 10 crops are supported while we got {}".format(test_crops))
       
   #Required transformations
   transform = torchvision.transforms.Compose([
@@ -133,42 +138,59 @@ def First_step():
   else:
       devices = list(range(args.workers))
     
-  model = torch.nn.DataParallel(model.cuda(devices[0]), device_ids=devices)
+  model_RGB = torch.nn.DataParallel(model_RGB.cuda(devices[0]), device_ids=devices)
+  model_RGBDiff = torch.nn.DataParallel(model_RGBDiff.cuda(devices[0]), device_ids=devices)
          
-  model.eval()    
+  model_RGB.eval()
+  model_RGBDiff.eval()     
 
   softmax = torch.nn.Softmax()
   scores = torch.tensor(np.zeros((1,101)), dtype=torch.float32).cuda()
    
   frames = []  
   frame_count = 0 
+  
   try: 
     top5_actions = Top_N(args.classInd_file)
     Tunnel_ = True
     conn,T_thr = Network.set_server(port=6666,Tunnel=Tunnel_,n=1)
     rcv_frames = Streaming.rcv_frames_thread(connection=conn[0])
     send_results = Streaming.send_results_thread(connection=conn[1])
+    
     while (rcv_frames.isAlive() and send_results.isAlive()):
         frame,status = rcv_frames.get()
         if frame is 0:
           break
-        frame_count += 1
+      
         frame = Image.fromarray(frame)
         
-        if args.modality == 'RGB':             
+        if frame_count % 12 < 6:
             frames.append(frame)
-            if frame_count % 5 == 0 and frame_count != 0:
-                frames = transform(frames).cuda()
-                scores = eval_video(frames)
-                scores = softmax(torch.FloatTensor(scores))
-                scores = scores.data.cpu().numpy().copy()
-                top5_actions.import_scores(scores[0,])
-                indecies,_,scores = top5_actions.get_top_N_actions()
-                send_results.put(status=status,scores=(*indecies,*scores))
-                
-                frames = []
-            else:
-                send_results.put(status=status)
+            print('loop, ', frame_count)
+      
+        if len(frames) == 18:       
+            frames = transform(frames).cuda()
+            scores_RGB = eval_video(frames[0:len(frames):6], 'RGB')   
+            scores_RGBDiff = eval_video(frames[:], 'RGBDiff')
+         
+            final_scores = args.score_weights[0]*scores_RGB + args.score_weights[1] * scores_RGBDiff
+            #final_scores = softmax(torch.FloatTensor(final_scores))
+            #final_scores = final_scores.data.cpu().numpy().copy()
+            
+            #five_scores = np.argsort(final_scores)[0][::-1][:5]
+            #action_checker = Evaluation(list(final_scores[0][five_scores]), psi=0.1)
+            
+            top5_actions.import_scores(final_scores[0,])
+            indices,_,scores = top5_actions.get_top_N_actions()
+            send_results.put(status=status,scores=(*indices,*scores))
+          
+            frames = [] 
+          
+        else:
+            send_results.put(status=status)
+            
+        frame_count += 1
+          
   except (KeyboardInterrupt,IOError,OSError):
     pass
   finally:

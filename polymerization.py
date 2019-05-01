92eb7ae4bdfec5526ec6d38d6dca8820fcc2b6cb
plebchan <ahelewhaatia@gmail.com>
2019-05-01 22:54:00 +0200

    adjusting error in parser line

--- polymerization.py
+++ polymerization.py
@@ -44,7 +44,7 @@ parser.add_argument('--psi', type=float, default=10)
 parser.add_argument('--test',dest = 'test',action='store_true',help='coloring the output scores with red color in case of NoActivity case')
 parser.add_argument('--p',dest= 'port' , type = int , default = 6666)
 parser.add_argument('--h',dest= 'hostname' , type = str , default = 'login01')
-parser.add_argument('--u',dest= 'hostname' , type = str , default = 'alex039u2')
+parser.add_argument('--u',dest= 'username' , type = str , default = 'alex039u2')
 
 args = parser.parse_args()
 
